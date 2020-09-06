9b9cfcca58232be9a7720f77e025ce12f4b5d828
anuragrao42 <anurag.18003@iitgoa.ac.in>
2020-09-06 23:44:38 +0530

    Updates

---
+++ precache-manifest.befc405377669ac525fd5421727d1150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c66e94a719f9dfe51e989e24d82ffd6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "011d84b7f0180b345013",
+    "url": "/monster-rolodex/static/css/main.150b6e51.chunk.css"
+  },
+  {
+    "revision": "4ec84f44fa1bcfd619ba",
+    "url": "/monster-rolodex/static/js/2.41d9bff7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.41d9bff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "011d84b7f0180b345013",
+    "url": "/monster-rolodex/static/js/main.c03a0d43.chunk.js"
+  },
+  {
+    "revision": "4c747da4b1ce0143ff44",
+    "url": "/monster-rolodex/static/js/runtime-main.69762926.js"
+  }
+]);
